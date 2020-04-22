ee57b6d11a865d469033b536a4be5381a7b29f43
vinicius707 <viniciussilva707@gmail.com>
2020-04-22 19:19:32 -0300

    implementing card

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -2,11 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatCardModule} from '@angular/material/card';
 
 const MaterialComponents = [
   MatButtonModule,
   BrowserAnimationsModule,
-  FormsModule
+  FormsModule,
+  MatInputModule,
+  MatToolbarModule,
+  MatCardModule
 ]
 
 @NgModule({
