c38b6d69ec767ead5ee8d57147a6e7b839ba9dc7
vinicius707 <viniciussilva707@gmail.com>
2020-04-22 13:49:12 -0300

    Separating functions by components

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,13 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { Todo } from '../todo'
+import { TodoDataService } from '../todo-data.service';
 
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
-  styleUrls: ['./tasks.component.css']
+  styleUrls: ['./tasks.component.css'],
+  providers: [TodoDataService]
 })
 export class TasksComponent implements OnInit {
 
-  constructor() { }
+  newTodo:Todo = new Todo();
+
+  constructor(private todoDataService: TodoDataService){
+
+  }
+
+  addTodo(){
+    this.todoDataService.addTodo(this.newTodo);
+    this.newTodo = new Todo();
+  }
+
+  toggleTodoComplete(todo) {
+    this.todoDataService.toggleTodoComplete(todo);
+  }
+
+  removeTodo(todo){
+    this.todoDataService.deleteTodoById(todo.id);
+  }
+
+  get todos(){
+    return this.todoDataService.getAllTodos();
+  }
 
   ngOnInit(): void {
   }
